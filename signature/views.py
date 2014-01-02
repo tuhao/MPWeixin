6d8460c4f3f4717c3a9a54758e10df46afd37970
unknown <astro.liuhang@gmail.com>
2014-01-02 09:47:06 +0800

    csrf_exempt

--- signature/views.py
+++ signature/views.py
@@ -3,12 +3,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from signature.models import *
+from django.views.decorators.csrf import csrf_exempt
 import hashlib
 import time
 import xml.etree.ElementTree as ET
 
 TOKEN = "ApesRise"
 
+@csrf_exempt
 def check_signature(request):
 	signature = request.REQUEST.get("signature",None)
 	timestamp = request.REQUEST.get("timestamp",None)
@@ -30,6 +32,7 @@ def check_signature(request):
 		#return reply_message(request)
 		return HttpResponse("invalid request")
 
+@csrf_exempt
 def reply_message(request):
 	try:
 		doc = ET.parse(request)
@@ -44,14 +47,8 @@ def reply_message(request):
 	else:
 		return HttpResponse("xml parse error")
 
-def reply_message_test(request):
-	try:
-		message = Message.objects.order_by('-id')[0]
-	except Exception, e:
-		return HttpResponse(e)
-	create_timestamp = int(time.time())
-	return render_to_response('reply_message.html',locals(),content_type="application/xml")
 
+@csrf_exempt
 def reply_news(request):
 	try:
 		doc = ET.parse(request)
@@ -67,6 +64,15 @@ def reply_news(request):
 		return render_to_response('reply_news.html',locals(),content_type="application/xml")
 	else:
 		return HttpResponse("xml parse error")
+
+
+def reply_message_test(request):
+	try:
+		message = Message.objects.order_by('-id')[0]
+	except Exception, e:
+		return HttpResponse(e)
+	create_timestamp = int(time.time())
+	return render_to_response('reply_message.html',locals(),content_type="application/xml")
 
 def reply_news_test(request):
 	try:
