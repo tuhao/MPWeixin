a69a342aa54e81d5e0c21ca7d01ca4293650f18d
root <astro.liuhang@gmail.com>
2014-01-07 15:42:32 +0000

    fix

--- signature/views.py
+++ signature/views.py
@@ -90,9 +90,11 @@ def reply(request):
 				if func is not None:
 					return func(request,param)
 				else:
-					key = key.encode('utf-8')
 					if key[:2] == 'ss':
 						key = key[2:]
+                                        if key.startswith('搜索'):
+	key=key[4:]
+                                                print key
 						return reply_search(request, param, key)
 					else:
 						return reply_help(request,param)
@@ -105,11 +107,11 @@ def reply(request):
 
 def reply_search(request,param,query):
 	if query is not None:
-		query = query.encode('utf-8')
 		try:
-			r = Message.search().query(query)
+			r = Message.search.query(query)
 			results = list(r)[:5]
 		except Exception, e:
+                        print e
 			return HttpResponse(e)
 		else:
 			return reply_gen_news(request, param, results)
